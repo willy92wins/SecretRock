ff4f414cd9c8e76ffbf87049469d1532160b20ce
willy92wins <guillermo_92@msn.com>
2026-09-14 13:12:15 +0200

    Snap kit hologram onto terrain instead of sky.

--- SecretRock/scripts/4_World/SecretRock_Hologram.c
+++ SecretRock/scripts/4_World/SecretRock_Hologram.c
@@ -1,5 +1,9 @@
 modded class Hologram
 {
+    protected const float SR_HOLO_GROUND_RAY_UP = 2.0;
+    protected const float SR_HOLO_GROUND_RAY_DOWN = 20.0;
+    protected const float SR_HOLO_GROUND_SNAP_Y = 0.05;
+
     protected bool SecretRock_IsKitProjection()
     {
         if (m_Parent)
@@ -12,6 +16,31 @@ modded class Hologram
         }
 
         return false;
+    }
+
+    protected vector SecretRock_SnapToTerrain(vector pos)
+    {
+        vector rayFrom = Vector(pos[0], pos[1] + SR_HOLO_GROUND_RAY_UP, pos[2]);
+        vector rayTo = Vector(pos[0], pos[1] - SR_HOLO_GROUND_RAY_DOWN, pos[2]);
+        vector groundHitPos;
+        vector groundHitNormal;
+        int groundComponent;
+        set<Object> groundResults = null;
+        Object gWith = m_Player;
+        Object gIgnore = m_Projection;
+        bool gSorted = false;
+        bool gGroundOnly = false;
+        float gRadius = 0.0;
+        bool groundHit = DayZPhysics.RaycastRV(rayFrom, rayTo, groundHitPos, groundHitNormal, groundComponent, groundResults, gWith, gIgnore, gSorted, gGroundOnly, ObjIntersectFire, gRadius);
+        if (groundHit)
+        {
+            vector snappedHit = Vector(groundHitPos[0], groundHitPos[1] + SR_HOLO_GROUND_SNAP_Y, groundHitPos[2]);
+            return snappedHit;
+        }
+
+        float surfaceY = g_Game.SurfaceY(pos[0], pos[2]);
+        vector snappedSurface = Vector(pos[0], surfaceY + SR_HOLO_GROUND_SNAP_Y, pos[2]);
+        return snappedSurface;
     }
 
     override string ProjectionBasedOnParent()
@@ -65,10 +94,11 @@ modded class Hologram
             {
                 vector depOffset = deployKit.GetDeployPositionOffset();
                 vector depFinal = position + depOffset;
+                vector snapped = SecretRock_SnapToTerrain(depFinal);
 
                 if (m_Projection)
                 {
-                    m_Projection.SetPosition(depFinal);
+                    m_Projection.SetPosition(snapped);
                 }
                 return;
             }
